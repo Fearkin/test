69bf60aa701ed1c3e3677347aaf5760206f410df
Fearkin <belyaev38@inbox.ru>
2021-03-25 15:25:39 +0300

    initial commit

---
+++ solver.js
@@ -0,0 +1,25 @@
+import { json } from "./constants";
+
+class Solver {
+
+    constructor(product) {
+        this.product = product;
+    }
+
+    getProductName() {
+        
+    }
+
+    getShops() {
+
+    }
+
+    getMaxAmount() {
+
+    }
+}
+
+const solver = new Solver(json);
+//solver.getProductName();
+//solver.getShops();
+//solver.getMaxAmount();
